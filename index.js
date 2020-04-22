8f22c287085ea34dd3c4f56899a562f408bb5a29
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2020-04-22 21:38:13 +0200

    add more lines

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 module.exports = function (msg) {
+  console.log(" * ".repeat(msg.length))
   console.log("*".repeat(msg.length))
   console.log(msg)
   console.log("*".repeat(msg.length))
+  console.log(" * ".repeat(msg.length))
 
 }
